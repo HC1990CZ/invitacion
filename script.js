f77f0c6037a8f9ad682ddffd38804edcab4c7da7
xoxogt <hugocumez1990@gmail.com>
2025-01-18 12:25:14 -0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", () => {
-    const video = document.getElementById("bg-video");
-    const scrollPosition = window.scrollY;
-
-    // Ajusta la posición del video según el desplazamiento
-    video.style.transform = `translateY(${scrollPosition * 0.5}px)`;
-});
